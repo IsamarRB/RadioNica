729c11fbf22348aa6ddba0311be385aa52f7a054
IsaRB <romerobellorini@gmail.com>
2025-03-30 11:38:13 +0200

    Added servlet/UsuarioServlet code

---
+++ src/main/java/com/radionica/servlets/UsuarioServlet.java
@@ -0,0 +1,28 @@
+package com.radionica.servlets;
+
+
+import com.radionica.dao.UsuarioDAO;
+import com.radionica.model.Usuario;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/usuarios")
+public class UsuarioServlet {
+    private final UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String nombre = request.getParameter("nombre");
+        String contraseña = request.getParameter("contraseña");
+        String rol = request.getParameter("rol");
+
+        Usuario usuario = new Usuario(nombre, contraseña, rol);
+        usuarioDAO.guardarUsuario(usuario);
+
+        response.getWriter().write("Usuario creado exitosamente");
+    }
+}
