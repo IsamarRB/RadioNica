da643e9a9ef8854bfad51ec6f4384c065e34bc44
IsaRB <romerobellorini@gmail.com>
2025-04-03 20:54:45 +0200

    Add UsuarioRepository code

---
+++ src/main/java/com/radionica/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.radionica.repository;
+
+import com.radionica.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByCorreo(String correo);
