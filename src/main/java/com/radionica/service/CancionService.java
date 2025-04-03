aeaaee23f94585d3d67110864e90b7fa623626f7
IsaRB <romerobellorini@gmail.com>
2025-04-03 20:47:20 +0200

    Added CancionService code

--- src/main/java/com/radionica/service/CancionService.java
+++ src/main/java/com/radionica/service/CancionService.java
@@ -1,4 +1,35 @@
 package com.radionica.service;
 
-public class CancionService {
+import com.radionica.model.Cancion;
+
+import java.util.Optional;
+
+@Service
+public class CancionService implements BaseService<Cancion> {
+
+    private final CancionRepository cancionRepository;
+
+    public CancionService(CancionRepository cancionRepository) {
+        this.cancionRepository = cancionRepository;
+    }
+
+    @Override
+    public List<Cancion> listarTodos() {
+        return cancionRepository.findAll();
+    }
+
+    @Override
+    public Optional<Cancion> buscarPorId(Long id) {
+        return cancionRepository.findById(id);
+    }
+
+    @Override
+    public Cancion guardar(Cancion cancion) {
+        return cancionRepository.save(cancion);
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        cancionRepository.deleteById(id);
+    }
 }
